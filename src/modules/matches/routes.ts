63c8ce248221f7e6f2aa52436373b76350a41a47
riazahmedshah <riyazsh360@gmail.com>
2025-07-31 15:28:54 +0530

    protected route

--- src/modules/matches/routes.ts
+++ src/modules/matches/routes.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { swipe } from "./controllers/createSwipe";
+import { authMiddleWare } from "../auth/middleware/authMiddleware";
 
 export const swipeRoutes = express.Router();
 
-swipeRoutes.post('/send', swipe);
+swipeRoutes.post('/send', authMiddleWare, swipe);
