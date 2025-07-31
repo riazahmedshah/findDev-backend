40b26a398a1f343808d17096f4957ebcfb4d2a28
riazahmedshah <riyazsh360@gmail.com>
2025-07-31 20:34:11 +0530

    improved/roboust swipe service

--- src/modules/matches/dto/swipe.dto.ts
+++ src/modules/matches/dto/swipe.dto.ts
@@ -1,8 +1,8 @@
 import { $Enums } from "@/generated/prisma";
 
 type SwipeIds = {
-  swiper_user_id: string;
   swiped_user_id:string;
+  swiper_user_id: string;
 }
 
 export interface createSwipeDTO extends SwipeIds {
--- src/modules/matches/services/swipe.service.ts
+++ src/modules/matches/services/swipe.service.ts
@@ -13,10 +13,40 @@ export const SwipeService = async(data:createSwipeDTO) => {
     if(!isUserExistToSwipe){
       throw new ServiceError(404,"USER_NOT_FOUND", `User does not exists with id: ${data.swiped_user_id}`)
     }
+
+    /* Prevent Swiping Back on a User Who Has Already Sent a "PENDING" Request */
+    // IMPORTANT: composite key ==> swipedUserId_swiperUserId
+    const isSwipeExistsBySwipedUser = await MatchingRepository.findSwipe(data.swiper_user_id, data.swiped_user_id);
+    if(isSwipeExistsBySwipedUser && isSwipeExistsBySwipedUser?.status == 'PENDING'){
+      throw new ServiceError(409, "CANNOT_SWIPE", "Cannot swipe on this user; they have already sent you a pending request. Please respond via your pending requests list.")
+    }
+
+
+    /* Checking: is Swiper already made a request to Swiped User.. */
+    // AGAIN IMPORTANT NOTE: composite key ==> swipedUserId_swiperUserId
     const isSwipeExists = await MatchingRepository.findSwipe(data.swiped_user_id, data.swiper_user_id);
-    
+
+    if(isSwipeExists){
+      throw new ServiceError(409,"ALREADY_SWIPE_RECORDED","You have already send a request to this user")
+    } else{
+      const newSatus = data.action === 'RIGHT' ? 'PENDING' : 'IGNORED';
+      await MatchingRepository.createSwipe({
+        swiped_user_id: data.swiped_user_id,
+        swiper_user_id:data.swiped_user_id,
+        action:data.action,
+        status:newSatus
+      });
+
+      /* 
+      if(data.action === 'RIGHT'){
+        FUTURE: Send Notification...
+      }
+      */
+    }
+
+    return { message: "SWIPE_RECORDED_SUCCESSFULLY." };
     
   } catch (error) {
-    
+    throw new ServiceError(500,"INTERNAL_SERVICE_ERROR","something went wrong")
   }
 }
