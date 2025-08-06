82e7a85ac1e1dadd3da851e78284899420e7a06b
riazahmedshah <riyazsh360@gmail.com>
2025-08-06 15:09:14 +0530

    initilaise create-connection-service

--- src/modules/matches/services/createConnection.ts
+++ src/modules/matches/services/createConnection.ts
@@ -1,3 +1,11 @@
-export async function createConnection(){
-  
+import { createConnectionDTO } from "../dto/connection.dto";
+import { ServiceError } from "../utils/ServiceError";
+
+export async function createConnection(data:createConnectionDTO){
+  try {
+    
+  } catch (error) {
+    if(error instanceof ServiceError) throw error;
+    throw new ServiceError(500,"INTERNAL_SERVICE_ERROR")
+  }
 }
