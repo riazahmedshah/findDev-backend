e49fcd4691bfb7f14dcb09d42380e0e495d492b6
riazahmedshah <riyazsh360@gmail.com>
2025-10-03 15:07:30 +0530

    minor change in signup api

--- src/modules/auth/controllers/signup.ts
+++ src/modules/auth/controllers/signup.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { signupSchema } from "../schema/authSchemas";
 import { ResponseHandler } from "@/utils/ResponseHandler";
 import { UserRepository } from "@/modules/user/repositories/UserRepository";
+import { AuthService } from "../utils/AuthService";
 
 export const signup = async (req:Request, res:Response) => {
   const {success, data, error} = await signupSchema.safeParseAsync(req.body)
@@ -14,9 +15,11 @@ export const signup = async (req:Request, res:Response) => {
       email:data.email,
       password:data.password
     })
-
+    
+    const token = AuthService.createToken(user.id);
     return ResponseHandler.created(res,{
-      MESSAGE:`${user.username} CREATED SUCCESSFULLY`
+      MESSAGE:`${user.username} CREATED SUCCESSFULLY`,
+      token
     })
   } catch (error) {
     return ResponseHandler.error(res,error)
